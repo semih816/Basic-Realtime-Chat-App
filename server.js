c892760ebd11aedb41e819cc0ec952ba81b205a4
Semih <80643417+semih816@users.noreply.github.com>
2023-07-10 13:15:16 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const socket = require('socket.io')
+
+const app = express()
+const server  = app.listen(3002)        
+
+app.use(express.static('public'))
+
+const io = socket(server)
+
+io.on('connection',(socket) =>{
+    console.log(socket.id);
+
+    socket.on('chat', data =>{
+        io.sockets.emit('chat' ,data)
+    })
+    socket.on('typing', data =>{
+        socket.broadcast.emit('typing',data)
+    })
+})
